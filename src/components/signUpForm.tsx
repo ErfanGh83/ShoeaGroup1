4f481532bf00c66b75154f785309f90ff8e6aed8
ErfanGh <erfan.ghasemian83@gmail.com>
2024-12-07 16:34:46 +0330

    created ui side of the form (validation with zod remains)

---
+++ src/components/signUpForm.tsx
@@ -0,0 +1,54 @@
+import { useForm } from "react-hook-form";
+import { FormData } from "./formTypes";
+import FormField from "./FormField";
+
+function Form() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setError,
+  } = useForm<FormData>();
+
+  const onSubmit = async (data: FormData) => {
+      console.log("SUCCESS", data);
+  }
+
+  return (
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="grid col-auto">
+          <h1 className="text-3xl font-bold mb-4">
+            Zod & React-Hook-Form
+          </h1>
+          <FormField
+            type="email"
+            placeholder="Email"
+            name="email"
+            register={register}
+            error={errors.email}
+          />
+
+          <FormField
+            type="password"
+            placeholder="Password"
+            name="password"
+            register={register}
+            error={errors.password}
+          />
+
+          <FormField
+            type="password"
+            placeholder="Confirm Password"
+            name="confirmPassword"
+            register={register}
+            error={errors.confirmPassword}
+          />
+          <button type="submit" className="submit-button">
+            Submit
+          </button>
+        </div>
+      </form>
+  );
+}
+
+export default Form;
--- src/pages/shoea.tsx
+++ src/pages/shoea.tsx
@@ -1,16 +1,18 @@
 import { useState } from "react";
 import LoadingPage from "./pageLoading"
 import WelcomePage from "./pageWelcom";
-import Onboarding from "../pages/Onboarding";
+import Onboarding from "./onboarding";
+import Form from "../components/signUpForm";
 
 const Shoea = () => {
-  const [page, setPage] = useState(0);
+  const [page, setPage] = useState(3);
 
   return (
     <>
       {page === 0 && <LoadingPage setPage={setPage} />}
       {page === 1 && <WelcomePage setPage={setPage} />}
       {page === 2 && <Onboarding />}
+      {page === 3 && <Form />}
     </>
   );
 };
