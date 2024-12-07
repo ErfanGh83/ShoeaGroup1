4c39029737d2dc7cd7f717d79ac965a25a8df405
ErfanGh <erfan.ghasemian83@gmail.com>
2024-12-07 16:25:34 +0330

    added types for sign up form

---
+++ src/components/formTypes.tsx
@@ -0,0 +1,24 @@
+import { FieldError, UseFormRegister } from "react-hook-form";
+
+export type FormData = {
+    username: string;
+    email: string;
+    password: string;
+    confirmPassword: string;
+  };
+
+  export type FormFieldProps = {
+    type: string;
+    placeholder: string;
+    name: ValidFieldNames;
+    register: UseFormRegister<FormData>;
+    error: FieldError | undefined;
+    valueAsNumber?: boolean;
+  };
+
+
+  export type ValidFieldNames =
+  | "username"
+  | "email"
+  | "password"
+  | "confirmPassword";
