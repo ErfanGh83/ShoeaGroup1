56736e48f3a8713f41b30ac2485cf28fd64b2d64
ErfanGh <erfan.ghasemian83@gmail.com>
2024-12-12 16:09:22 +0330

    home container's categories section almost complete

--- src/components/HomeComponents/homeContainer/homeContainerCategories.component.tsx
+++ src/components/HomeComponents/homeContainer/homeContainerCategories.component.tsx
@@ -3,8 +3,51 @@
 const HomeContainerCategories = () => {
 
     return(
-        <div className="w-full h-[250px] flex flex-wrap bg-red-400">
-            <h1>categories</h1>
+        <div className="w-full h-[250px] border-2 border-black">
+            <ul className="w-full h-full grid grid-cols-4 grid-rows-2">
+                <li className="w-[60px] h-[91px] bg-slate-400">
+                    <div className="size-[60px] rounded-full bg-slate-300">
+
+                    </div>
+
+                    <h2 className="text-center font-semibold mt-2">
+                        Nike
+                    </h2>
+                    
+                </li>
+
+                <li className="w-[60px] h-[91px] bg-slate-400">
+                    <div className="size-[60px] rounded-full bg-slate-300">
+
+                    </div>
+
+                    <h2 className="text-center font-semibold mt-2">
+                        Nike
+                    </h2>
+                    
+                </li>
+                <li className="w-[60px] h-[91px] bg-slate-400">
+                    <div className="size-[60px] rounded-full bg-slate-300">
+
+                    </div>
+
+                    <h2 className="text-center font-semibold mt-2">
+                        Nike
+                    </h2>
+                    
+                </li>
+                <li className="w-[60px] h-[91px] bg-slate-400">
+                    <div className="size-[60px] rounded-full bg-slate-300">
+
+                    </div>
+
+                    <h2 className="text-center font-semibold mt-2">
+                        Nike
+                    </h2>
+                    
+                </li>
+                
+            </ul>
         </div>
     )
 }
