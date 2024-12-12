bd3159a868233e42c4e7794c6cdeaf5ba8d58057
ErfanGh <erfan.ghasemian83@gmail.com>
2024-12-12 16:35:44 +0330

    added products component to home container and fetched products list with axios

--- src/components/HomeComponents/homeContainer.component.tsx
+++ src/components/HomeComponents/homeContainer.component.tsx
@@ -1,10 +1,12 @@
 import HomeContainerCategories from "./homeContainer/homeContainerCategories.component"
+import HomeContainerProductsComponent from "./homeContainer/homeContainerProducts.component"
 
 const HomeContainer = () => {
 
     return(
-        <div className="w-11/12 h-full my-4 border-2 border-black mx-auto flex flex-col">
+        <div className="w-11/12 h-full my-4 border-2 mx-auto flex flex-col">
             <HomeContainerCategories />
+            <HomeContainerProductsComponent />
         </div>
     )
 }
