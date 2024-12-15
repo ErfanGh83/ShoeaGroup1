adcf16308e67a46f571f6a74d564b9974b77c374
ErfanGh <erfan.ghasemian83@gmail.com>
2024-12-15 11:39:58 +0330

    completed ui part of the product page

--- src/pages/productPage.page.tsx
+++ src/pages/productPage.page.tsx
@@ -3,6 +3,9 @@ import { useEffect, useState } from "react";
 import { BiArrowBack } from "react-icons/bi";
 import { BiHeart } from "react-icons/bi";
 import { BiStar } from "react-icons/bi";
+import { BiPlus } from "react-icons/bi";
+import { BiMinus } from "react-icons/bi";
+import { BiShoppingBag } from "react-icons/bi";
 import axios from "axios";
 
 function ProductPage() {
@@ -69,6 +72,111 @@ function ProductPage() {
                     View more...
                 </button>
             </p>
+        </div>
+
+        <div className="flex flex-row px-6 justify-between mt-2">
+            <div className="flex flex-col w-1/2">
+                <p className="text-2xl font-bold">
+                    Size
+                </p>
+
+                <ul className="w-11/12 h-fit py-2 flex flex-row gap-4 overflow-x-scroll">
+                    <li className="size-10 rounded-full border-2 border-black items-center">
+                        <p className="size-10 px-2 pt-1 font-bold">
+                            42
+                        </p>
+                    </li>
+
+                    <li className="size-10 rounded-full border-2 border-black items-center">
+                        <p className="size-10 px-2 pt-1 font-bold">
+                            43
+                        </p>
+                    </li>
+
+                    <li className="size-10 rounded-full border-2 border-black items-center">
+                        <p className="size-10 px-2 pt-1 font-bold">
+                            44
+                        </p>
+                    </li>
+                    
+                </ul>
+            </div>
+
+            <div className="flex flex-col w-1/2">
+                <p className="text-2xl font-bold">
+                    Color
+                </p>
+
+                <ul className="w-11/12 h-fit py-2 flex flex-row gap-4 overflow-x-scroll">
+                    <li className="size-10 rounded-full bg-red-600 shadow-md">
+                        <p className="size-10 px-2 pt-1 font-bold ">
+                            
+                        </p>
+                    </li>
+
+                    <li className="size-10 rounded-full bg-blue-600 shadow-md">
+                        <p className="size-10 px-2 pt-1 font-bold ">
+                            
+                        </p>
+                    </li>
+
+                    <li className="size-10 rounded-full bg-green-600 shadow-md">
+                        <p className="size-10 px-2 pt-1 font-bold ">
+                            
+                        </p>
+                    </li>
+
+                    <li className="size-10 rounded-full bg-white shadow-md">
+                        <p className="size-10 px-2 pt-1 font-bold ">
+                            
+                        </p>
+                    </li>
+
+                    <li className="size-10 rounded-full bg-purple-600 shadow-md">
+                        <p className="size-10 px-2 pt-1 font-bold ">
+                            
+                        </p>
+                    </li>
+                    
+                </ul>
+            </div>
+        </div>
+
+        <div className="flex flex-row px-6 mt-8 items-center ">
+            <p className="text-2xl font-bold">Qauntity</p>
+
+            <div className="w-[150px] h-[50px] flex flex-row justify-evenly items-center rounded-full mx-4 bg-gray-200 shadow-lg">
+                <button>
+                    <BiMinus size={24} />
+                </button>
+
+                <p className="text-xl font-semibold">
+                    0
+                </p>
+
+                <button>
+                    <BiPlus size={24}/>
+                </button>
+            </div>
+        </div>
+
+        <hr className="mx-6 mt-6" />
+
+        <div className="flex flex-row px-6 my-10 justify-between">
+            <div>
+                <p className="text-base text-gray-500 font-medium">
+                    Total price
+                </p>
+
+                <p className="text-2xl font-bold">
+                    $259.99
+                </p>
+            </div>
+
+            <button className="w-[300px] h-[60px] flex flex-row items-center rounded-full bg-black text-white justify-center gap-2 shadow-sm">
+                <BiShoppingBag size={24}/>
+                <p  className="text-2xl font-semibold">Add to cart</p>
+            </button>
         </div>
     </div>
   )
