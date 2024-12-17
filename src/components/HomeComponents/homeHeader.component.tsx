b24d5c3be6e38ce6efd0ec653b734f902efcd104
ErfanGh <erfan.ghasemian83@gmail.com>
2024-12-17 11:04:53 +0330

    changed static welcome username in home page, now it welcomes logged in users by their username

--- src/components/HomeComponents/homeHeader.component.tsx
+++ src/components/HomeComponents/homeHeader.component.tsx
@@ -1,22 +1,36 @@
 import { BiBell } from "react-icons/bi";
 import { BiHeart } from "react-icons/bi";
+import { useNavigate } from "react-router-dom";
+import { CgProfile } from "react-icons/cg";
 
 const HomeHeader = ()=>{
+
+    const navigate = useNavigate()
+    const storedUser = localStorage.getItem("user");
+    let username;
+
+    if (storedUser) {
+        const user = JSON.parse(storedUser);
+        username = user.username;
+    }
+    else{
+        username = '';
+    }
 
     return(
         <header className="w-full h-20 shadow-xl absolute top-0 flex flex-row justify-between items-center px-6">
             <div className="w-[194px] h-12 justify-between flex flex-row">
-                <div className="w-12 h-12 rounded-full bg-black">
-                    
+                <div className="size-12 flex rounded-full justify-center items-center">
+                    <CgProfile size={50}/>
                 </div>
                 
                 <div className="flex flex-col w-[140px] ml-2">
                     <p className="text-gray-400 text-md">
-                        Good Morning 👋
+                        {username? 'Good Morning 👋' : ''}
                     </p>
 
                     <h2 className=" font-semibold text-lg">
-                        Saeed Abdilar
+                        {username? username : <button className="m-auto" onClick={()=>navigate('/login')}>Login</button>}
                     </h2>
                 </div>
             </div>
