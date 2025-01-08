324119638ba011cf9cf247aab70b1294d563384d
ali abdolvandd <aliabdolvandaa76@gmail.com>
2025-01-09 01:57:25 +0330

    add component show products in checkout page

--- src/pages/checkoutPage.tsx
+++ src/pages/checkoutPage.tsx
@@ -1,9 +1,12 @@
 // import AddressComponent from "../components/checkout/addressComponent";
 // import DeliveryComponent from "../components/checkout/deliveryComponent";
 
+import CheckoutProducts from "../components/checkout/checkoutProducts";
+
 function CheckoutPage() {
   return (
     <div>
+      {<CheckoutProducts />}
       {/* <AddressComponent /> */}
       {/* <DeliveryComponent /> */}
     </div>
